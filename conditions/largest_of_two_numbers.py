b6a7217297ca3b22b6f0c2f6402d7c5c44b96bc8
Saheerkhan S <maheebadhusha@gmail.com>
2026-06-12 22:29:48 +0530

    Add largest_of_two_numbers.py to compare two numbers

---
+++ conditions/largest_of_two_numbers.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+if n1 > n2:
+    print("First number is largest")
+else:
+    print("Second number is largest")
