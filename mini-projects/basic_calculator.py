8ec7b8c488b815a40233a3b9c224fedc54545e30
Saheerkhan S <maheebadhusha@gmail.com>
2026-06-12 21:18:40 +0530

    Add basic calculator functionality in Python

---
+++ mini-projects/basic_calculator.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Sum of two numbers:", num1 + num2)
+print("Difference of two numbers:", num1 - num2)
+print("Product of two numbers:", num1 * num2)
